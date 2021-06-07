79c2a6efe88c972fde892e5b3ed95cf25343df1f
Christina <ckuo2001518@gmail.com>
2021-06-07 13:32:36 +0800

    added comments

--- project/server.R
+++ project/server.R
@@ -132,10 +132,15 @@ shinyServer(function(input, output) {
     
     #----------------------------------Start of Page 4-------------------------------#
     
+    ## Text for the page's introduction
     output$intro_text <- renderText({
         paste0("The table on this page can show the strong relationship between happiness and GDP of a country.")
     })
     
+    ## Table and text for the three happiest countries with their GDP
+    output$happiest_text <- renderText({
+        paste0("Table of Happiest Countries in ", input$year)
+    })
     output$happiest <- renderTable({
         happiest <- data %>% 
             select(Year, Overall.rank, Country, Score, GDP.per.capita) %>% 
@@ -144,6 +149,10 @@ shinyServer(function(input, output) {
             head(3)
     })
     
+    ## Table for the three unhappiest countries with their GDP
+    output$unhappiest_text <- renderText({
+        paste0("Table of least happy Countries in ", input$year)
+    })
     output$unhappiest <- renderTable({
         unhappiest <- data %>% 
             select(Year, Overall.rank, Country, Score, GDP.per.capita) %>% 
@@ -152,39 +161,31 @@ shinyServer(function(input, output) {
             head(3)
     })
     
-    output$happiest_text <- renderText({
-        paste0("Table of Happiest Countries in ", input$year)
-    })
-    
-    output$unhappiest_text <- renderText({
-        paste0("Table of least happy Countries in ", input$year)
-    })
-    
+    ## Table and text for the table of average GDP
     output$gdp_text <- renderText({
         paste0("The average GDP each year")
     })
-    
     output$average_GDP <- renderTable({
         averageGDP_df <- data %>% 
             group_by(Year) %>% 
             summarize(Average_GDP = mean(GDP.per.capita))
     })
     
+    ## First summary
     output$summary1_page4 <- renderText({
         paste0("Comparing the two tables, we found that the GDP of the three happiest countries are all higher than the average GDP, 
                while the GDP of the least happy countries are lower than the average GDP.")
     })
-    
     output$summary2_page4 <- renderText({
         paste0("Additionally, we summarized the growth of the score and GDP of each country between 2015 and 2019. 
                We found that the five countries that have the most increase on happiness have either the same GDP
                or a rising GDP.")
     })
     
+    ## Table and text for the table of countries that increased their happiness the most
     output$increase_text <- renderText({
         paste0("Table of Countries that their happiness increased the most between 2015 and 2019")
     })
-    
     output$increase <- renderTable({
         increase <- data %>% 
             select(Year, Overall.rank, Country, Score, GDP.per.capita) %>% 
@@ -195,6 +196,7 @@ shinyServer(function(input, output) {
             head(5)
     })
     
+    ## Last summary
     output$summary3_page4 <- renderText({
         paste0("Looking at the tables, we can conclude that GDP has a strong relationship with a country's happiness score.
                 Moreover, the happiness score is proportional to GDP in most cases.")
